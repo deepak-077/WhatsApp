e7f5ff59179620a815c5a8631fa3b5e7c064d128
deepak-077 <itsdepak@gmail.com>
2025-09-12 14:35:26 +0530

    chat window added

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
 import Chat from "@/components/Chats";
 import Header from "@/components/Header";
+import Chatwindow from "@/components/Chatwindow";
 
 export default function Home() {
   return (
     //main container
-    <div className="bg-[#161717] flex flex-col gap-2">
+    <div className="bg-[#161717] flex w-screen">
 
       {/* chats */}
       <div className="max-w-[408.8px] w-full ">
         <Header/>
         <Chat/>      
+        
 
       </div>
 
       {/* chat window */}
-      <div>
+        <Chatwindow/>
 
-      </div>
+      
 
 
 
--- components/Chatwindow.tsx
+++ components/Chatwindow.tsx
@@ -1,6 +1,42 @@
 function Chatwindow(){
     return(
-        <div className="max-w-[]">
+        <div className="max-w-[890.2] w-full py-[28px] h-screen text-white flex flex-col gap-2 items-center justify-center border border-amber-300">
+
+            <div className="max-w-[560px] w-full h-[310px] mt-[-20px]">
+
+            {/* image */}
+            <div className="max-w-[560px] w-full h-[152px] flex justify-center">
+                <img className="max-w-[228px] h-[152px] " src="whatsapp.png" alt="img" />
+
+            </div>
+
+            <div className="max-w-[560px] w-full h-[134px] flex flex-col items-center text-center">
+                <div className="max-w-[560px] w-full h-[36px] mt-[24px] mb-[12px] text-4xl font-medium">
+                    Download WhatsApp for Mac
+                </div>
+
+                <div className="max-w-[560px] w-full h-[16px] mb-[24px] font-medium ">
+                    Make calls and get a faster experience when you download the Mac app.
+                </div>
+                
+                <div className="max-w-[560px]  w-full h-[38px] mt-[32px] ">
+                   <button className="max-w-[177.7px]  w-full h-[38px] bg-[#21c063] text-sm text-black font-semibold rounded-3xl">
+                    Get from App Store
+                   </button>
+                </div>
+                
+            </div>
+            
+            </div>
+
+            <div className="h-[22.3px] text-sm text-white">
+                    <img src="" alt="" />
+                    <div className="">Your personal messages are end-to-end encrypted</div>
+            </div>
+            
+
+            
+
 
         </div>
 
